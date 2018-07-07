84f7a47240ab0046548e1c028b1aa0f57207b0a5
Alex Orga <alexorga.dev@gmail.com>
2018-07-07 11:31:33 -0500

    created a public routing function

--- src/App/App.js
+++ src/App/App.js
@@ -10,7 +10,7 @@ import Navbar from '../components/Navbar/Navbar';
 // import New from '../components/New/New';
 // import Order from '../components/Order/Order';
 // import OrderSpa from '../components/OrderSpa/OrderSpa';
-// import Register from '../components/Register/Register';
+import Register from '../components/Register/Register';
 // import SingleOrder from '../components/SingleOrder/SingleOrder';
 
 const PrivateRoute = ({component: Component, authed, ...rest}) => {
@@ -23,6 +23,24 @@ const PrivateRoute = ({component: Component, authed, ...rest}) => {
         ) : (
           <Redirect
             to={{pathname: '/login', state: {from: props.location}}}
+          />
+        )
+      }
+
+    />
+  );
+};
+
+const PublicRoute = ({component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === false ? (
+          <Component {...props}/>
+        ) : (
+          <Redirect
+            to={{pathname: '/orders', state: {from: props.location}}}
           />
         )
       }
@@ -52,6 +70,11 @@ class App extends Component {
                     component={Inventory}
                   />
                 </Switch>
+                <PublicRoute
+                  path="/register"
+                  authed={this.state.authed}
+                  component={Register}
+                />
               </div>
             </div>
           </div>
