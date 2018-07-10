4bdb541f5da9225aef4035d51b64885fefc2200a
Alex Orga <alexorga.dev@gmail.com>
2018-07-09 19:26:10 -0500

    added a component for displaying inventory names from firebase

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,12 +1,37 @@
 import React from 'react';
 
+import fishRequests from '../../firebaseRequests/fishes';
+
 import './Inventory.css';
 
 class Inventory extends React.Component {
+  state = {
+    fishes: [],
+  }
+
+  componentDidMount () {
+    fishRequests
+      .getRequest()
+      .then((fishes) => {
+        this.setState({fishes: fishes});
+      })
+      .catch((err) => {
+        console.error('error loading fishes');
+      });
+  }
+
   render () {
+    const fishComponents = this.state.fishes.map((fish) => {
+      return (
+        <h2>{fish.name}</h2>
+      );
+    });
     return (
       <div className="Inventory text-center">
         <h1>Inventory</h1>
+        <ul className="fishes">
+          {fishComponents}
+        </ul>
       </div>
     );
   }
